3c8408db40d206501dc049042c979a20ee26a044
Truperton <58820346+Truperton@users.noreply.github.com>
2024-12-29 20:14:23 +0000

    Update Ship_Controls.gd

--- Scripts/Ship_Controls.gd
+++ Scripts/Ship_Controls.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var lmbDown : bool = false
 var mouseMoved : bool = false
+var mousePosition : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,24 +11,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if lmbDown:
+		position = mousePosition
 
 func _input(event):
 	# Local Variables
 
 	# Main _input()
-	
-	# Mouse in viewport coordinates.
+	# Checks which mouse buttons are pressed
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			lmbDown = true
 		else:
 			lmbDown = false
-	if event is InputEventMouseMotion:
-		mouseMoved = true
-	else:
-		mouseMoved = false
 	
-	if lmbDown and mouseMoved:
-		position = event.position
-		print("Moving Player")
+	# Updates mousePosition
+	if event is InputEventMouseMotion:
+		mousePosition = event.position
