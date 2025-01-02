3eddd5cf7c6852162fe39a7a890f1aab2a35326f
Truperton <58820346+Truperton@users.noreply.github.com>
2025-01-02 12:44:27 +0000

    Added background and ability to shoot
    As well as what is mentioned in the title I have changed the formatting of the code a bit to fit better with the GDScript style guide. An obstacle I have realized when adding the background is that it is a bit too distracting, so I will need to find a way to make the stuff happening in the level actually pop out more.

--- Scripts/Ship_Controls.gd
+++ Scripts/Ship_Controls.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
-var lmbDown : bool = false
-var mouseMoved : bool = false
-var mousePosition : Vector2
+@export var speed: float = 200
+@export var projectile_scene: PackedScene
+@export var shoot_cooldown: float = 1
+
+var lmb_down: bool = false
+var mouse_moved: bool = false
+var mouse_position: Vector2
+
+var shoot_timer: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +17,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if lmbDown:
-		position = mousePosition
+	if lmb_down:
+		#var direction = position.direction_to(mousePosition)
+		#position += direction * speed * delta
+		position = mouse_position
+		if shoot_timer > shoot_cooldown:
+			shoot()
+			shoot_timer = 0
+	shoot_timer += delta
 
 func _input(event):
 	# Local Variables
@@ -21,10 +33,18 @@ func _input(event):
 	# Checks which mouse buttons are pressed
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			lmbDown = true
+			lmb_down = true
 		else:
-			lmbDown = false
+			lmb_down = false
 	
 	# Updates mousePosition
 	if event is InputEventMouseMotion:
-		mousePosition = event.position
+		mouse_position = event.position
+
+func shoot() -> void:
+	# Local Variables
+	var spawned_projectile = projectile_scene.instantiate()
+	# Main _fire()
+	spawned_projectile.global_position = global_position + Vector2(0, -20)
+	
+	get_tree().current_scene.add_child(spawned_projectile)
