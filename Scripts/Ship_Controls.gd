e8c5f1891b019e7fd1246e90f08dfe451f644916
Trouperton <58820346+Trouperton@users.noreply.github.com>
2025-01-11 16:40:30 +0000

    Overhauled controls and started updating firing mechanic

--- Scripts/Ship_Controls.gd
+++ Scripts/Ship_Controls.gd
@@ -1,12 +1,11 @@
 extends Area2D
 
 @export var speed: float = 200
-@export var projectile_scene: PackedScene
+@export var primary_fire_projectile_scene: PackedScene
 @export var shoot_cooldown: float = 1
+@export var Barrels: Array[Node2D]
 
-var lmb_down: bool = false
-var mouse_moved: bool = false
-var mouse_position: Vector2
+var movement_direction: Vector2
 
 var shoot_timer: float = 0
 
@@ -17,34 +16,52 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if lmb_down:
-		#var direction = position.direction_to(mousePosition)
-		#position += direction * speed * delta
-		position = mouse_position
-		if shoot_timer > shoot_cooldown:
-			shoot()
-			shoot_timer = 0
+	position += movement_direction.normalized() * speed * delta
 	shoot_timer += delta
 
 func _input(event):
 	# Local Variables
-
-	# Main _input()
-	# Checks which mouse buttons are pressed
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			lmb_down = true
-		else:
-			lmb_down = false
 	
-	# Updates mousePosition
-	if event is InputEventMouseMotion:
-		mouse_position = event.position
+	# Main _input()
+	if event is InputEventKey and event.is_pressed():
+		# The following 4 if statements handle pressed down keyboard movement input
+		if event.is_action_pressed("up"):
+			movement_direction.y -= 1
+		if event.is_action_pressed("down"):
+			movement_direction.y += 1
+		if event.is_action_pressed("left"):
+			movement_direction.x -= 1
+		if event.is_action_pressed("right"):
+			movement_direction.x += 1
+		
+		if event.is_action_pressed("primary_fire"):
+			$PrimaryFireCooldownTimer.start()
+	elif event is InputEventKey and event.is_released():
+		# The following 4 if statements handle released keyboard movement input
+		if event.is_action_released("up"):
+			movement_direction.y += 1
+		if event.is_action_released("down"):
+			movement_direction.y -= 1
+		if event.is_action_released("left"):
+			movement_direction.x += 1
+		if event.is_action_released("right"):
+			movement_direction.x -= 1
+		if event.is_action_released("primary_fire"):
+			$PrimaryFireCooldownTimer.stop()
 
 func shoot() -> void:
 	# Local Variables
-	var spawned_projectile = projectile_scene.instantiate()
-	# Main _fire()
-	spawned_projectile.global_position = global_position + Vector2(0, -20)
+	#var spawned_projectile = primary_fire_projectile_scene.instantiate()
 	
-	get_tree().current_scene.add_child(spawned_projectile)
+	# Main shoot()
+	for i in Barrels:
+		var spawned_projectile = primary_fire_projectile_scene.instantiate()
+		
+		spawned_projectile.global_position = i.global_position
+		get_tree().current_scene.add_child(spawned_projectile)
+	#spawned_projectile.global_position = global_position + Vector2(0, -20)
+	
+	#get_tree().current_scene.add_child(spawned_projectile)
+
+func _on_primary_fire_cooldown_timer_timeout() -> void:
+	shoot()
