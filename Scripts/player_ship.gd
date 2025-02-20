0b31d4839cc1dab9f63697ef2af3f31d44a2c8c0
Trouperton <truperunit1@gmail.com>
2025-02-20 18:59:01 +0000

    Update player_ship.gd
    Added comments

--- Scripts/player_ship.gd
+++ Scripts/player_ship.gd
@@ -5,13 +5,14 @@ func _process(delta: float) -> void:
 	# Local Variables
 	
 	# Main _process()
+	## Moves the player based on the move_direction vector
 	position += move_direction * move_speed * delta
 
 func _input(event):
 	# Local Variables
 	
 	# Main _input()
-	## This if statement handles player movement
+	## This if statement handles player movement input
 	if event is InputEventKey or event is InputEventJoypadMotion:
 		move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
@@ -28,10 +29,16 @@ func _on_primary_fire_cooldown_timer_timeout() -> void:
 	# Main _on_primary_fire_cooldown_timer_timeout()
 	shoot_primary()
 
+## Overrides the base class' damage function.
+## The base classes code is executed first, after which the code specific to
+## the player is executed. Such as the rumble and health bar update.
 func damage(damage_dealt: int):
+	# Local Variables
+	
+	# Main damage()
 	super.damage(damage_dealt)
 	
-	#if $"HUD".is_inside_tree():
-		#print("Found HUD")
+	## Briefly rumbles the weak motor on a controller
 	Input.start_joy_vibration(0, 0.5, 0, 0.2)
+	## Updates the health bar by calling the appropriate method on the HUD
 	get_parent().get_node("HUD").update_health_bar((float(health) / health_max) * 100)
